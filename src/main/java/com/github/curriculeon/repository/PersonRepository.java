627fe73b59f0fa7e9277449f6a937696368f4d69
leon hunter <xleonhunter@gmail.com>
2020-01-10 16:34:43 -0500

    update

--- src/main/java/com/github/curriculeon/repository/PersonRepository.java
+++
@@ -1,9 +0,0 @@
-package com.github.curriculeon.repository;
-
-import com.github.curriculeon.model.Person;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PersonRepository extends CrudRepository<Person, Long> {
-}
